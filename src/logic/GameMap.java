3c575062eb8611b4a63e0f1a6ae751ceaddea9f8
Pongsapak Pulthasthan <jamebond73@hotmail.com>
2020-04-16 00:08:51 +0700

    Update GameMap.java

--- src/logic/GameMap.java
+++ src/logic/GameMap.java
@@ -10,6 +10,28 @@ public class GameMap {
 		setWidth(column);
 		
 		cellmap = new Cell[row][column];
+		for(int i=0; i<row; i++) {
+			for(int j=0; j<column; j++) {
+				cellmap[i][j] = new Cell(); 
+			}
+		}
+	}
+	
+	public GameMap(String [][] map) {
+		int row = map.length;
+		int column = map[0].length;
+		setHeight(row);
+		setWidth(column);
+		
+		cellmap = new Cell[row][column];
+		for(int i=0; i<row; i++) {
+			for(int j=0; j<column; j++) {
+				cellmap[i][j] = new Cell(); 
+				
+			}
+		}
+		
+		
 		
 	}
 
