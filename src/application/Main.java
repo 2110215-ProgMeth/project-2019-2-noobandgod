2a2a85c761e30711859e633b1a1488ee78631e62
Pongsapak Pulthasthan <jamebond73@hotmail.com>
2020-04-17 18:47:14 +0700

    Add x,y field to Entity / Edit GameMap.java

--- src/application/Main.java
+++ src/application/Main.java
@@ -6,24 +6,8 @@ public class Main{
 
 	public static void main(String[] args) {
 		
-		String[][] a = {{"aa","ab"},{"ba","bb"}};
-		for(int i=0; i<2; ++i) {
-			for(int j=0; j<2; ++j) {
-				System.out.println(a[i][j]);
-			}
-		}
-		
-		Scanner kb = new Scanner(System.in);
-		String c = kb.next();
-		String[] cc = c.split(",");
-		
-		for (String d: cc) {
-			System.out.println(d);
-		}
-		
-		//Hello
-		
-		
+		String[][] gamemap = CSVParser.readCSV("Book1.csv");
+		System.out.println(gamemap.toString());
 		
 		
 	}
--- src/logic/GameMap.java
+++ src/logic/GameMap.java
@@ -1,27 +1,24 @@
 package logic;
 
+import java.util.ArrayList;
+
+import entity.base.Entity;
+
 public class GameMap {
 	private int width;
 	private int height;
 	private Cell[][] cellmap;
+
+	private ArrayList<Entity> allEntity;
 	
-	public GameMap(int row, int column) {
-		setHeight(row);
-		setWidth(column);
-		
-		cellmap = new Cell[row][column];
-		for(int i=0; i<row; i++) {
-			for(int j=0; j<column; j++) {
-				cellmap[i][j] = new Cell(); 
-			}
-		}
-	}
 	
 	public GameMap(String [][] map) {
+		allEntity = new ArrayList<Entity>();
 		int row = map.length;
 		int column = map[0].length;
 		setHeight(row);
 		setWidth(column);
+		
 		
 		cellmap = new Cell[row][column];
 		for(int i=0; i<row; i++) {
@@ -50,6 +47,16 @@ public class GameMap {
 	public void setHeight(int height) {
 		this.height = height;
 	}
+	
+	public boolean addBlock(Entity e,int x,int y) {
+		allEntity.add(e);
+		
+		
+		
+		
+		
+	}
+	
 	
 	
 	
