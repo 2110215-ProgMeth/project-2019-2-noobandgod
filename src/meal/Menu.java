8942e16b03317a1090beca8009b47889e94ea8aa
Pongsapak Pulthasthan <jamebond73@hotmail.com>
2020-04-18 23:50:36 +0700

    Update Menu.java

--- src/meal/Menu.java
+++ src/meal/Menu.java
@@ -1,5 +1,29 @@
 package meal;
 
-public class Menu {
+import java.util.ArrayList;
 
+import entity.Ingredient;
+
+public abstract class Menu {
+	protected String name;
+	protected int price;
+	protected int max_score;
+	protected ArrayList<Ingredient> ingredients;
+	protected int timeleft;
+	
+	public Menu(int timeleft) {
+		setTimeleft(timeleft);
+	}
+	
+	public abstract String toString();
+	public abstract String getName();
+	public abstract boolean isAllIngredients();
+
+	public int getTimeleft() {
+		return timeleft;
+	}
+
+	public void setTimeleft(int timeleft) {
+		this.timeleft = timeleft;
+	}
 }
