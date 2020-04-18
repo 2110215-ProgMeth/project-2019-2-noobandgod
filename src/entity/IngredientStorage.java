ff735597456b9b3045ac94d7cdc0bb794ad25a51
Millennium133 <aong1244@gmail.com>
2020-04-18 16:31:54 +0700

    IngredientStorage
    เหมือนกับ ingredient ตรงที่ยังไม่มี throw exception แล้วก็ยังไม่ได้กำหนดให้stationที่มีของอยู่แล้ว หายไป(ของด้านบนstation)

---
+++ src/entity/IngredientStorage.java
@@ -0,0 +1,8 @@
+package entity;
+
+import entity.base.Entity;
+import entity.base.Interactable;
+
+public abstract class IngredientStorage extends Entity implements Interactable {
+	public abstract Entity
+}
