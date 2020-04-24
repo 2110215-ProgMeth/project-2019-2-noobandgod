c1709685f97008ed927074b78ada442ba6466691
Pongsapak Pulthasthan <jamebond73@hotmail.com>
2020-04-25 00:55:08 +0700

    last commit for today

--- src/gui/ShopPane.java
+++ src/gui/ShopPane.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import javafx.beans.InvalidationListener;
 import javafx.beans.Observable;
 import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
@@ -12,6 +13,7 @@ import javafx.scene.text.Font;
 
 public class ShopPane extends VBox {
 	private ObservableList<IngredientShopBox> ingredientShopBoxs = FXCollections.observableArrayList();
+	private Label totalpriceLabel;
 	
 	public ShopPane(String[] ingredientName) {
 		this.setPrefHeight(600);
@@ -24,28 +26,26 @@ public class ShopPane extends VBox {
 			this.getChildren().add(ingredientShopBox);
 		}
 		
-		Label totalpriceLabel = new Label("Total price:  "+"0");
+		this.totalpriceLabel = new Label("Total price:  "+"0");
 		totalpriceLabel.setFont(new Font(16));
 		
-		ingredientShopBoxs.addListener(new InvalidationListener() {
-			@Override
-			public void invalidated(Observable arg0) {
-				int price = calculateTotalPrice();
-				totalpriceLabel.setText("Total price:  "+price);
-			}
-		});
+		
+		
+		
+		
 		
 		
 		this.getChildren().add(totalpriceLabel);
 	}
 	
-	public int calculateTotalPrice() {
+	public void calculateTotalPrice() {
 		int totalmoney = 0;
 		for (IngredientShopBox i: this.ingredientShopBoxs) {
 			int price = i.getIngredientItem().getPrice();
 			int amount = i.getAmountBox().getAmount();
 			totalmoney += price*amount;
 		}
-		return totalmoney;
+		totalpriceLabel.setText("Total price:  "+totalmoney);
+		
 	}
 }
