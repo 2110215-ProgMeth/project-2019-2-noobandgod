e3b92dd1e7d210399361ce454c8fbceca186fcb5
Millennium133 <aong1244@gmail.com>
2020-04-29 21:20:12 +0700

    Update Main2.java

--- src/application/Main2.java
+++ src/application/Main2.java
@@ -3,6 +3,8 @@ package application;
 import gui.IngredientStoragePane;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import screen.StartScreen;
@@ -11,14 +13,15 @@ public class Main2 extends Application {
 
 	@Override
 	public void start(Stage primaryStage) {
-		StartScreen start = new StartScreen();
-		Scene scene = new Scene(start);
-		scene.setFill(Color.RED);
-		
-		primaryStage.setScene(scene);
-		
-		primaryStage.setResizable(false);
-		primaryStage.show();
+//		StartScreen start = new StartScreen();
+//		Scene scene = new Scene(start);
+//		scene.setFill(Color.RED);
+//		primaryStage.setScene(scene);
+//		primaryStage.setResizable(false);
+//		primaryStage.show();
+		Canvas canvas = new Canvas(800, 400);
+		GraphicsContext gc = canvas.getGraphicsContext2D();
+		primaryStage.getChildren().add(canvas);
 	}
 
 	public static void main(String[] args) {
