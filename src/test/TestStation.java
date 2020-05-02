58ba8f08e02401c8fc4bcdd90d3e8cd8c9a30513
Millennium133 <aong1244@gmail.com>
2020-05-02 17:54:03 +0700

    TestStation

--- src/test/TestStation.java
+++ src/test/TestStation.java
@@ -19,20 +19,49 @@ public class TestStation {
 	Dish dish;
 	Cabbage cabbage;
 	public void setUp() {
-		player1 = new Player(0,0,0);
-		player2 = new Player(1,0,0);
+		cabbage = new Cabbage();
 		dish = new Dish();
+		
+		player1 = new Player(0,0,0);
+		
+		player2 = new Player(1,0,0);
 		player2.setEntityHeld(dish);
+		
+		player3 = new Player(2,0,0);
+		player3.setEntityHeld(cabbage);
+		
+		
 		station1  = new Station();
+		
+		station2 = new Station();
+		station2.setOnStationExists(dish);
 
+		station3 = new Station();
+		station3.setOnStationExists(cabbage);
 
 	}
 		@Test
-//		public void testConstructorStation() {
-//			assertEquals(null,player1.getEntityHeld());
-//	}
-		public void testConstructorPlayer() {
-			assertEquals(null,player1.getEntityHeld());
-			assertEquals(false,player1.isHolding());
-		}
+	public void testConstructorStation() {
+		assertEquals(false,station1.interacts(player1));
+		assertEquals(true,station1.interacts(player2));
+		assertEquals(true,station1.interacts(player3));
+		assertEquals(true,station2.interacts(player1));
+		assertEquals(false,station2.interacts(player2));
+		assertEquals(true,station2.interacts(player3));
+		assertEquals(true,station3.interacts(player1));
+		assertEquals(true,station3.interacts(player2));
+		assertEquals(false,station3.interacts(player3));
+			
+	}
+	public void testConstructorPlayer() {
+		assertEquals(false,player1.isHolding());
+		assertEquals(null,player1.getEntityHeld());
+		assertEquals(true,player2.isHolding());
+		assertEquals(true,player2.getEntityHeld() instanceof Dish);
+		assertEquals(false,player3.isHolding());
+		assertEquals(true,player3.getEntityHeld() instanceof Cabbage);
+	}
+	public void testStation1() {
+	
+	}
 }
