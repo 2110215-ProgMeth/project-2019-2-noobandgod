b9c3735321eb83b011c9ee7cc1b194a949e473a8
Millennium133 <aong1244@gmail.com>
2020-05-02 18:52:58 +0700

    Edit in interface

--- src/entity/base/Consumable.java
+++
@@ -1,8 +0,0 @@
-package entity.base;
-
-import entity.Player;
-import exception.ConsumeFailedException;
-
-public interface Consumable {
-	public abstract boolean consumes(Player e) throws ConsumeFailedException;
-}
