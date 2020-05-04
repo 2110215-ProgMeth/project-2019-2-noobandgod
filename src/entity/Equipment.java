4fc65087d3200061a6179cba1c715a72bfa9cae5
Pongsapak Pulthasthan <jamebond73@hotmail.com>
2020-05-05 00:49:28 +0700

    update animationTimner (progessBar) in Equipment

--- src/entity/Equipment.java
+++ src/entity/Equipment.java
@@ -14,27 +14,38 @@ public abstract class Equipment extends Block implements Interactable,Cookable{
 	protected boolean isWorking;
 	
 	//not used yet
-	public void drawProgessBar(GraphicsContext gc) {
+	public void drawProgessBar(GraphicsContext gc, int maxTime) {
+		final long startNanoTime = System.nanoTime();
+		
 		int pixel = GameScreen.pixel;
 		int x = GameScreen.draw_origin_x+this.getX()*pixel;
 		int y = GameScreen.draw_origin_y+this.getY()*pixel-7;
 		
-		final long startNanoTime = System.nanoTime();
+		double maxwidth = 64;
+		double maxheight = 5;
+		
 		AnimationTimer animationTimer = new AnimationTimer() {
+			double width = 0;
 			@Override
 			public void handle(long currentNanoTime) {
 				double t = ((currentNanoTime - startNanoTime) / 1000000000.0);
-				int maxwidth = GameScreen.pixel;
-				int width = (int) (t/5)*maxwidth;
-				int height = 5;
+				width = (t/maxTime)*maxwidth;
 				
+				gc.clearRect(x, y, maxwidth, maxheight);
 				
-				gc.setFill(Color.WHITE);
-				gc.fillRect(0, 0, width, height);
+				gc.setFill(Color.ALICEBLUE);
+				gc.fillRect(x, y, maxwidth , maxheight);
 				
+				gc.setFill(Color.GREEN);
+				gc.fillRect(x, y, width, maxheight);
 				
+				if (width >= maxwidth) {
+					gc.clearRect(x, y, maxwidth, maxheight);
+					this.stop();
+				}
 			}
 		};
+		animationTimer.start();
 		
 	}
 }
