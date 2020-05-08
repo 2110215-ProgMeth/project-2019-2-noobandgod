2ed02d9cc704b3075e56206e72499165932d4667
Pongsapak Pulthasthan <jamebond73@hotmail.com>
2020-05-08 15:42:55 +0700

    edit padding in orderPane

--- src/gui/OrderPane.java
+++ src/gui/OrderPane.java
@@ -3,16 +3,19 @@ package gui;
 import java.util.ArrayList;
 
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.layout.HBox;
 import logic.GameController;
 import meal.Menu;
 import meal.OrderManager;
+
 public class OrderPane extends HBox {
 	public OrderPane() {
 		this.setPrefHeight(192);
 		this.setPrefWidth(800);
 		this.setSpacing(6);
-		this.setPadding(new Insets(4));
+		this.setPadding(new Insets(0,0,0,6));
+		//this.setAlignment(Pos.CENTER);
 		
 		for (Menu menu : GameController.getOrderManager().getOrders()) {
 			OrderBox order = new OrderBox(menu);
