628971da465ed517164d28803da113a6ae32ac72
Millennium133 <aong1244@gmail.com>
2020-05-10 14:38:18 +0700

    Game

--- src/sharedObject/AudioLoader.java
+++ src/sharedObject/AudioLoader.java
@@ -9,10 +9,10 @@ public class AudioLoader {
 	public static final AudioClip ERRORR = new AudioClip(ClassLoader.getSystemResource("sound/rror.mp3").toString());
 	public static final AudioClip CONGRAT = new AudioClip(ClassLoader.getSystemResource("sound/congratulation.mp3").toString());
 	public static final AudioClip SUCCESS_SEND = new AudioClip(ClassLoader.getSystemResource("sound/SuccessSend.mp3").toString());
-	
-	public static final Song START_SONG = new Song(1);
-	public static final Song GAME_SONG = new Song(2);
-	public static final Song END_SONG = new Song(3);
+//	
+//	public static final Song START_SONG = new Song(1);
+//	public static final Song GAME_SONG = new Song(2);
+//	public static final Song END_SONG = new Song(3);
 	
 	public static final AudioClip END = new AudioClip(ClassLoader.getSystemResource("sound/EndScreen.mp3").toString());
 	
